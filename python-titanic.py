2252a2e71ead59b790776b009c0818ebbb55872f
Anthony Okoth <misc4tony089@gmail.com>
2018-03-30 15:43:01 +0200

    modelling and submission

--- python-titanic.py
+++ python-titanic.py
@@ -2,9 +2,16 @@ import caret as cr
 import pandas as pd
 import seaborn as sns
 import matplotlib as plt
+from sklearn import tree
 
 
 trainClean = pd.read_csv("trainClean.csv", encoding = "utf-8-sig")
 testClean = pd.read_csv("testClean.csv", encoding= "utf-8-sig")
 
-print sns.heatmap(trainClean.isnull(), cmap="viridis", cbar=False, yticklabels=False)
+giniModel = tree.DecisionTreeClassifier(criterion="gini")
+entropyModel = tree.DecisionTreeClassifier(criterion="entropy")
+chiModel = tree.DecisionTreeClassifier(criterion="chi")
+
+giniModel.fit(trainClean, trainClean["Survived"])
+giniPredicted = giniModel.predict(testClean)
+print(giniPredicted)
